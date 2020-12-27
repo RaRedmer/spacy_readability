d63fde422630b5552fe6cb93f92a2f4657f55c23
44050496 <ra.redmer@outlook.com>
2020-12-27 13:16:27 +0100

    Added parameterization

--- spacy_readability/__init__.py
+++ spacy_readability/__init__.py
@@ -15,7 +15,7 @@ from spacy_syllables import SpacySyllables
 
 
 from .words import DALE_CHALL_WORDS
-from .available_measures import MEASURES
+from .measure_parameters import MEASURE_PARAMETERS
 
 
 class Readability(SpacySyllables):
@@ -43,20 +43,19 @@ class Readability(SpacySyllables):
         """Initialise the pipeline component.
         """
         super(Readability, self).__init__(nlp, lang=lang)
-
         lang = lang or nlp.lang
         # take only supported measures
         if measures:
-            measures = set(MEASURES[lang]) & set(measures)
+            self.measures = {metric: MEASURE_PARAMETERS[lang][metric] for metric in set(MEASURE_PARAMETERS[lang].keys()) & set(measures)}
         else:
-            measures = MEASURES[lang]
+            self.measures = MEASURE_PARAMETERS[lang]
 
         for metric in ["total_sentences", "total_words", "total_syllables", "total_letters"]:
             Doc.set_extension(metric, default=None, force=True)
 
         Token.set_extension("letters_count", default=None, force=True)
 
-        for metric in measures:
+        for metric in self.measures.keys():
             if not Doc.has_extension(metric):
                 Doc.set_extension(metric, getter=getattr(self, metric))
 
@@ -84,53 +83,25 @@ class Readability(SpacySyllables):
         doc._.set("total_letters", total_letters)
         return doc
 
-    def total_sentences(self, doc: Doc):
-        """Return total number of sentences in the document
-        """
-        return sum(1 for sent in doc.sents)
-
-    def total_words(self, doc: Doc):
-        """Return total number of words in the document.
-        Filters punctuation and words that start with apostrophe (aka contractions)
-        """
-        return sum(1 for word in doc if not word.is_punct)
-
-    def total_syllables(self, doc: Doc):
-        """Return total number of syllables in the document.
-        """
-        return sum(token._.syllables_count for token in doc if (
-                not token.is_punct
-                and not token.is_digit
-            )
-        )
-
-    def total_letters(self, doc: Doc):
-        """Return total number of syllables in the document.
-        """
-        return sum(token._.letters_count for token in doc if (
-                not token.is_punct
-                and not token.is_digit
-            )
-        )
-
-    def flesch_kincaid_grade_level(self, doc):
+    def flesch_kincaid_grade_level(self, doc: Doc):
         """Returns the Flesch-Kincaid grade for the document.
         """
-        # num_sentences = _get_num_sentences(doc)
+        params = self.measures["flesch_kincaid_grade_level"]
         num_sentences = doc._.total_sentences
         num_words = doc._.total_words
         num_syllables = doc._.total_syllables
         if num_sentences == 0 or num_words == 0 or num_syllables == 0:
             return 0
         return (
-            (11.8 * num_syllables / num_words)
-            + (0.39 * num_words / num_sentences)
-            - 15.59
+            (params["asw"] * num_syllables / num_words)
+            + (params["asl"] * num_words / num_sentences)
+            - params["base"]
         )
 
-    def flesch_kincaid_reading_ease(self, doc):
+    def flesch_kincaid_reading_ease(self, doc: Doc):
         """Returns the Flesch-Kincaid Reading Ease score for the document.
         """
+        params = self.measures["flesch_kincaid_reading_ease"]
         num_sentences = doc._.total_sentences
         num_words = doc._.total_words
         num_syllables = doc._.total_syllables
@@ -138,11 +109,12 @@ class Readability(SpacySyllables):
             return 0
         words_per_sent = num_words / num_sentences
         syllables_per_word = num_syllables / num_words
-        return 206.835 - (1.015 * words_per_sent) - (84.6 * syllables_per_word)
+        return params["base"] - (params["asl"] * words_per_sent) - (params["asw"] * syllables_per_word)
 
-    def dale_chall(self, doc):
+    def dale_chall(self, doc: Doc):
         """Returns the Dale-Chall score for the document.
         """
+        params = self.measures["dale_chall"]
         num_sentences = doc._.total_sentences
         num_words = doc._.total_words
         if num_sentences == 0 or num_words == 0:
@@ -156,26 +128,28 @@ class Readability(SpacySyllables):
         )
         percent_difficult_words = 100 * diff_words_count / num_words
         average_sentence_length = num_words / num_sentences
-        grade = 0.1579 * percent_difficult_words + 0.0496 * average_sentence_length
+        grade = params["pdw"] * percent_difficult_words + params["asl"] * average_sentence_length
 
         # if percent difficult words is about 5% then adjust score
         if percent_difficult_words > 5:
             grade += 3.6365
         return grade
 
-    def smog(self, doc):
+    def smog(self, doc: Doc):
         """Returns the SMOG score for the document. If there are less than 30 sentences then
         it returns 0 because he formula significantly loses accuracy on small corpora.
         """
+        params = self.measures["smog"]
         num_sentences = doc._.total_sentences
         num_words = doc._.total_words
         if num_sentences < 30 or num_words == 0:
             return 0
         num_poly = _get_num_syllables(doc, min_syllables=3)
-        return 1.0430 * sqrt(num_poly * 30 / num_sentences) + 3.1291
+        return params["mult"] * sqrt(num_poly * 30 / num_sentences) + params["base"]
 
-    def coleman_liau_index(self, doc):
+    def coleman_liau_index(self, doc: Doc):
         """Returns the Coleman-Liau index for the document."""
+        params = self.measures["coleman_liau_index"]
         num_words = doc._.total_words
         if num_words <= 0:
             return 0
@@ -186,10 +160,12 @@ class Readability(SpacySyllables):
             return 0
         letters_to_words = letter_count / num_words * 100
         sent_to_words = num_sentences / num_words * 100
-        return 0.0588 * letters_to_words - 0.296 * sent_to_words - 15.8
+        return params["l"] * letters_to_words - params["s"] * sent_to_words - params["base"]
 
-    def automated_readability_index(self, doc):
-        """Returns the Automated Readability Index for the document."""
+    def automated_readability_index(self, doc: Doc):
+        """Returns the Automated Readability Index for the document.
+        """
+        params = self.measures["automated_readability_index"]
         num_sentences = doc._.total_sentences
         num_words = doc._.total_words
         if num_words <= 0:
@@ -198,11 +174,12 @@ class Readability(SpacySyllables):
         letter_count = doc._.total_letters
         letter_to_words = letter_count / num_words
         words_to_sents = num_words / num_sentences
-        return 4.71 * letter_to_words + 0.5 * words_to_sents - 21.43
+        return params["alw"] * letter_to_words + params["asw"] * words_to_sents - params["base"]
 
-    def forcast(self, doc):
+    def forcast(self, doc: Doc):
         """Returns the Forcast score for the document.
-        """
+        """            
+        params = self.measures["forcast"]
         sample_size = 150
         num_words = doc._.total_words
         eligible_words = [word for word in doc if not word.is_punct and not word.is_digit]
@@ -210,7 +187,21 @@ class Readability(SpacySyllables):
             return 0
         mono_syllabic = sum(1 for idx in random_sample(num_words, size=sample_size, replace=True)
                                 if eligible_words[idx]._.syllables_count == 1)
-        return 20 - (mono_syllabic / 10)
+        return params["base"] - (mono_syllabic / params["w"])
+
+    def gunning_fog_index(self, doc: Doc):
+        """Returns the Gunning Fog Index score for the document.
+        """
+        params = self.measures["gunning_fog_index"]
+        words_to_sents = doc._.total_words / doc._.total_sentences
+        # count words containing atleast 3 syllables
+        complex_words = sum(1 for token in doc if (
+            not token.is_punct
+            and not token.is_digit
+            and token._.syllables_count
+            and token._.syllables_count >= 3))
+        complex_words_proportion = complex_words / doc._.total_words
+        return params["mult"] * (words_to_sents + 100 * complex_words_proportion)
 
 
 def _get_num_syllables(doc: Doc, min_syllables: int = 1):
---
+++ spacy_readability/measure_parameters.py
@@ -0,0 +1,46 @@
+MEASURE_PARAMETERS = {
+    "en": {
+        "flesch_kincaid_grade_level":{
+            "base": 15.59,
+            "asl": 0.39,
+            "asw": 11.8,
+        },
+        "flesch_kincaid_reading_ease": {
+            "base": 206.835,
+            "asl": 1.015,
+            "asw": 84.6,
+        },
+        "dale_chall": {
+            "pdw": 0.1579,
+            "asl": 0.0496,
+        },
+        "smog": {
+            "mult": 1.0430,
+            "base": 3.1291,
+        },
+        "coleman_liau_index": {
+            "l": 0.0588,
+            "s": 0.296,
+            "base": 15.8,
+        },
+        "automated_readability_index": {
+            "alw": 4.71,
+            "asw": 0.5,
+            "base": 21.43,
+        },
+        "forcast": {
+            "base": 20,
+            "w": 10,
+        },
+        "gunning_fog_index": {
+            "mult": 0.4,
+        }
+    },
+    "de": {
+        "flesch_kincaid_reading_ease": {
+            "base": 180,
+            "asl": 1,
+            "asw": 58.5,
+        },
+    },
+}
